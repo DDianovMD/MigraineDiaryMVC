a3a72b13e10477f2962c673bb0741aad307ad4a2
DDianovMD <ddianovmd@abv.bg>
2023-04-16 13:29:37 +0300

    Seeded Admin user and role

--- src/MVCProject.Web/Data/ApplicationDbContext.cs
+++ src/MVCProject.Web/Data/ApplicationDbContext.cs
@@ -35,6 +35,46 @@ namespace MigraineDiary.Web.Data
                 .OnDelete(DeleteBehavior.NoAction);
 
             base.OnModelCreating(modelBuilder);
+
+            const string ADMIN_ID = "ff3d52a7-7288-42aa-9955-6c4c4ad4caed";
+            const string ROLE_ID = "2d75eec2-411b-43d0-acb7-5ae4bf74555f";
+
+            string path = Path.Combine(AppContext.BaseDirectory, "adminpassword.txt");
+            string password = File.ReadAllText(path);
+
+            PasswordHasher<ApplicationUser> passwordHasher = new PasswordHasher<ApplicationUser>();
+
+            modelBuilder.Entity<ApplicationUser>()
+                .HasData(new ApplicationUser
+                {
+                    Id = ADMIN_ID,
+                    UserName = "Admin",
+                    NormalizedUserName = "ADMIN",
+                    Email = "admin@migrainediary.com",
+                    NormalizedEmail = "ADMIN@MIGRAINEDIARY.COM",
+                    EmailConfirmed = true,
+                    PasswordHash = passwordHasher.HashPassword(null, password),
+                    LockoutEnabled = true,
+                    LockoutEnd = null,
+                    AccessFailedCount = 0,
+                    TwoFactorEnabled = false,
+                    IsDeleted = false,
+                });
+
+            modelBuilder.Entity<IdentityRole>()
+               .HasData(new IdentityRole
+               {
+                   Id = ROLE_ID,
+                   Name = "Admin",
+                   NormalizedName = "ADMIN"
+               });
+
+            modelBuilder.Entity<IdentityUserRole<string>>()
+                .HasData(new IdentityUserRole<string>()
+                {
+                    RoleId = ROLE_ID,
+                    UserId = ADMIN_ID,
+                });
         }
 
         public DbSet<Headache> Headaches { get; set; }
@@ -43,7 +83,7 @@ namespace MigraineDiary.Web.Data
 
         public DbSet<HIT6Scale> HIT6Scales { get; set; }
 
-        public DbSet<ZungScaleForAnxiety> ZungScalesForAnxiety { get; set;}
+        public DbSet<ZungScaleForAnxiety> ZungScalesForAnxiety { get; set; }
 
         public DbSet<Article> Articles { get; set; }
     }
