34ad4c09a6aa969a29a26d15faa62877c2f49617
DDianovMD <ddianovmd@abv.bg>
2023-05-31 10:30:29 +0300

    Implemented Share button and fixed CSS.

--- src/MVCProject.Web/wwwroot/js/search.js
+++ src/MVCProject.Web/wwwroot/js/search.js
@@ -24,8 +24,9 @@
                         let pElement = document.createElement('p');
 
                         // Set element's attributes.
+                        pElement.setAttribute('id', `searchResult-${i}`);
                         pElement.setAttribute('class', 'searchResult');
-                        pElement.setAttribute('onclick', `removeSearchResults(${counterValue});`);
+                        pElement.setAttribute('onclick', `removeSearchResults(${counterValue}, ${i});`);
                         pElement.innerText = `Д-р ${jsonResult[i].firstName} ${jsonResult[i].lastName}`;
 
                         // Append element to DOM tree.
@@ -45,6 +46,7 @@
                     // Set element's attributes.
                     pElement.setAttribute('class', 'not-found-result');
                     pElement.style.textDecorationColor = 'red';
+                    pElement.style.marginLeft = '83px';
                     pElement.innerText = `Не са открити резултати.`;
 
                     // Append element to DOM tree.
@@ -61,13 +63,34 @@
 }
 
 
-function removeSearchResults(counterValue) {
+function removeSearchResults(counterValue, resultCounter) {
+    let resultValue;
+
     // Get div element in which results are nested.
     let searchResultDiv = document.getElementById(`searchResult-${counterValue}`);
 
+    // Get "Share" button element.
+    let shareButton = document.querySelector('.share-headache-btn');
+
+    // Check if resultCounter has value.
+    // Function is called with not null parameter only if clicked over presented on screen search result.
+    if (resultCounter != null) {
+        // Get clicked result innerText
+        resultValue = document.getElementById(`searchResult-${resultCounter}`).innerText;
+
+        // Set searchBar innerText as result's innerText.
+        document.getElementById(`searchBar-${counterValue}`).value = resultValue;
+
+        // Make "Share" button visible.
+        shareButton.style.visibility = 'visible';
+    } else {
+        // Clear searchBar text.
+        document.getElementById(`searchBar-${counterValue}`).value = '';
+
+        // Make "Share" button invisible.
+        shareButton.style.visibility = 'hidden';
+    }
+
     // Clear search results.
     searchResultDiv.innerHTML = '';
-
-    // Clear searchBar text.
-    document.getElementById(`searchBar-${counterValue}`).value = '';
 }
