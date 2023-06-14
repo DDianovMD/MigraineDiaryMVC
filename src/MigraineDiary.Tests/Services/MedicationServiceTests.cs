c64d9d14803909c887b64e543e7f103c8ad8c020
DDianovMD <ddianovmd@abv.bg>
2023-06-14 14:27:42 +0300

    Implemented unit tests for MedicationService.

---
+++ src/MigraineDiary.Tests/Services/MedicationServiceTests.cs
@@ -0,0 +1,32 @@
+﻿using MigraineDiary.Services;
+using MigraineDiary.Services.Contracts;
+using NUnit.Framework;
+
+namespace MigraineDiary.Tests.Services
+{
+    [TestFixture]
+    public class MedicationServiceTests
+    {
+        private IMedicationService medicationService;
+
+        [SetUp]
+        public void SetUp() 
+        {
+            this.medicationService = new MedicationService();
+        }
+
+        [TestCase(500, 3)]
+        [TestCase(200, 1)]
+        [TestCase(0, 1)]
+        public void CalculateWholeTakenDosage_MultiplicatesAsExpected(decimal singlePillDosage, decimal numberOfPillsTaken)
+        {
+            // Assert - in Setup method.
+
+            // Act
+            decimal result = this.medicationService.CalculateWholeTakenDosage(singlePillDosage, numberOfPillsTaken);
+
+            // Assert
+            Assert.That(result, Is.EqualTo(singlePillDosage * numberOfPillsTaken));
+        }
+    }
+}
